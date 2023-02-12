c50fb293957c0c1ecf795ac4833cd2c858260413
Maxime Muhlethaler <maxime.muhlethaler@gmail.com>
2023-02-12 14:50:03 +0100

    premier code Metropolis Hastings

--- MCMC1.py
+++ MCMC1.py
@@ -7,7 +7,7 @@ Created on Sat Feb 11 15:27:40 2023
 import math
 import numpy as np
 from matplotlib import pyplot as plt
-def f(x):   
+def f(x):
     return (1/np.sqrt(2*np.pi))*np.exp(-(x**2)/2)
 def f2(x):
     return np.exp(-(abs(x)))/2
@@ -19,23 +19,23 @@ def q(x,x_n):
 
 
 def acceptation(prop,x_n):
-    alpha=min(1,(f2(prop)*q(x_n,prop))/(f2(x_n)*q(prop,x_n)))
+    alpha=min(1,(f(prop)*q(x_n,prop))/(f(x_n)*q(prop,x_n)))
     u=np.random.uniform(0,1)
     if u<=alpha:
         return prop
     else:
         return x_n
-    
-    
-  
-    
+
+
+
+
 def MH(x0,N):
-    
+
     l=[x0]
     for k in range (N-1):
         x0=np.random.normal(l[-1],1)
         l.append(acceptation(x0,l[-1]))
-        
+
     l=np.array(l)
     print(l)
     plt.hist(l,bins=50,density=True ,color="lightblue")
@@ -43,7 +43,4 @@ def MH(x0,N):
     y=f(l2)
     plt.plot(l2,y)
     plt.show()
-#%%
-x0=np.random.normal(0,1,10000)
-plt.hist(x0,bins=50 ,density=True ,color="lightblue")
-    
+
