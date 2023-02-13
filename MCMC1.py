082da34cdf38d122bf996b7fb551c7ca70eaa3ac
Maxime Muhlethaler <maxime.muhlethaler@gmail.com>
2023-02-13 15:25:12 +0100

    Metroplis Hastings

--- MCMC1.py
+++ MCMC1.py
@@ -7,21 +7,31 @@ Created on Sat Feb 11 15:27:40 2023
 import math
 import numpy as np
 from matplotlib import pyplot as plt
-nu=0
+import scipy.integrate
+#paramètres pour siumuler des lois normales
+nu=10
 sigma=4
+#parametres pour la simulation et la chaîne de Markov
+dt=0.1
+#on conseille de prendre N>=10^5
+
+
+#on définit plusieurs fonctions tests que l'on veut échantilloner
 def f(x):
-    return (1/np.sqrt(2*np.pi*(sigma**2)))*np.exp(-((x-nu)**2)/(2*(sigma**2)))
+    return np.exp(-((x-nu)**2)/(2*(sigma**2)))
 def f2(x):
-    return np.exp(-(abs(x)))/2
+    return np.exp(-(abs(x)))
+def V(x):
+    return np.exp(-np.cos(2*np.pi*x))
 
-
+#on définit notre fonction instrumentale, on prend ici une gaussienne
 def q(x,x_n):
     return (1/math.sqrt(2*math.pi))*math.exp(-((x-x_n)**2)/2)
 
 
-
-def acceptation(prop,x_n):
-    alpha=min(1,(f(prop)*q(x_n,prop))/(f(x_n)*q(prop,x_n)))
+#on définit la fonction acceptation qui accepte ou rejette une proposition selon sa valeur
+def acceptation(prop,x_n,fonct):
+    alpha=min(1,(fonct(prop)*q(x_n,prop))/(fonct(x_n)*q(prop,x_n)))
     u=np.random.uniform(0,1)
     if u<=alpha:
         return prop
@@ -30,21 +40,26 @@ def acceptation(prop,x_n):
 
 
 
-
-def MH(x0,N):
-
+#on définit la foncion suivant la méthode Metropolis-Hastings ou l'on calcule les élements de notre suite de Markov, on trace ensuite un histogramme que l'on compare avec la fonction test
+def MH(x0,N,fonct):
     l=[x0]
     for k in range (N-1):
-        x0=np.random.normal(l[-1],1)
-        l.append(acceptation(x0,l[-1]))
-
+        x0=np.random.normal(0,1)*np.sqrt(2*dt)+l[-1]
+        l.append(acceptation(x0,l[-1],fonct))
     l=np.array(l)
-    print(l)
-    plt.hist(l,bins=50,density=True ,color="lightblue")
-    l2=np.arange(-5*sigma+nu,nu+5*sigma,0.001)
-    y=f(l2)
+    if (fonct==V):
+        l=abs(l)%1
+        plt.hist(l,bins=50,density=True ,color="lightblue")
+        l2=np.arange(0,1,0.001)
+        A=scipy.integrate.quad(fonct,0,1)[0]
+    else:
+        l2=np.arange(-5*sigma+nu,nu+5*sigma,0.001)
+        plt.hist(l,bins=50,density=True ,color="lightblue")
+        A=scipy.integrate.quad(fonct,-100,100)[0]
+    y=fonct(l2)/A
     plt.plot(l2,y)
     plt.show()
+
 
 
 
